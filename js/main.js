b1dc999e531cb080d36480c65b824fff44bf73f7
lauramunozcaiced <munoz.caicedo@hotmail.com>
2023-09-14 20:13:20 -0500

    yes

--- js/main.js
+++ js/main.js
@@ -2,16 +2,30 @@ const openEls = document.querySelectorAll("[data-open]");
 const closeEls = document.querySelectorAll("[data-close]");
 const isVisible = "is-visible";
 var vid = document.getElementById("video");
+const question = [
+  'Cuál ha sido tu mejor recuerdo durante 27 años y por qué?',
+  'Piensa en cinco cosas que quisiste tener o ser hasta ahora lo has logrado. No importa lo mínimo que sea', 
+  'Cómo quieres vivir?',
+  'Que le dirías al Nicolas del pasado, del presente y del futuro', 
+  'Algo que quieras dejar de tus 26 años', 
+  'Cierra los ojos y como si apagaras una velita de cumpleaños, pide un deseo',
+  'Date las gracias por algo o varias cosas',
+  'Cómo te ves en un año?',
+  'Que huella te gustaría dejar en el mundo?'
+]
 
 for (const el of openEls) {
   el.addEventListener("click", function() {
     const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
-    console.log(modalId);
-    if(modalId == 'modal2'){
+    if(modalId == 'modalVid'){
       vid.autoplay = true;
       vid.load();
+    }else if(modalId == 'modal'){
+      document.getElementById('question').innerText = question[el.dataset.number]
     }
+    document.getElementById(modalId).classList.add(isVisible);
+    console.log(modalId);
+    
     
   });
 }
